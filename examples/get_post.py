b89b9ff8874c8b1072f0b2cfe06359c2513d7507
barsikus007 <barsikus07@gmail.com>
2024-07-24 10:52:23 +0300

    trigger workflow

--- examples/get_post.py
+++ examples/get_post.py
@@ -8,6 +8,8 @@ async def main():
     response = await api.get_post("boosty", post_id="c9fb8a19-c45e-4602-9942-087c3af28c1b")
     print(response.title)
     # 'Добро пожаловать на борт!'
+    print(response.url)
+    # 'https://boosty.to/boosty/posts/c9fb8a19-c45e-4602-9942-087c3af28c1b'
 
 
 if __name__ == "__main__":
