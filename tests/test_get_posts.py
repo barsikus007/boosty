a9d5990f815397b38dd0d28c6de46a3f620b44fc
barsikus007 <barsikus07@gmail.com>
2026-09-20 18:01:51 +0300

    update test env

--- tests/test_get_posts.py
+++ tests/test_get_posts.py
@@ -6,18 +6,18 @@ from boosty.api import API
 pytest_plugins = ("pytest_asyncio",)
 
 
-@pytest_asyncio.fixture(scope="module")
+@pytest_asyncio.fixture(scope="module", loop_scope="module")
 async def api():
     return API()
 
 
-@pytest.mark.asyncio(scope="module")
+@pytest.mark.asyncio(loop_scope="module")
 async def test_get_specific_post(api: API):
     response = await api.get_post("boosty", post_id="c9fb8a19-c45e-4602-9942-087c3af28c1b")
     assert response.title == "Добро пожаловать на борт!"
 
 
-@pytest.mark.asyncio(scope="module")
+@pytest.mark.asyncio(loop_scope="module")
 async def test_get_all_posts(api: API):
     response = await api.get_posts("boosty")
     assert len(response.data) == 100
